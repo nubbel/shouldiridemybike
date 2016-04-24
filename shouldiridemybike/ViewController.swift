efcaeeea405104bc54bdf556a4d9649f6785d873
Dominique d'Argent <dargent@in.tum.de>
2016-04-24 19:20:06 +0200

    Initial Commit

---
+++ shouldiridemybike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shouldiridemybike
+//
+//  Created by Dominique d'Argent on 24/04/16.
+//  Copyright © 2016 Dominique d'Argent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
